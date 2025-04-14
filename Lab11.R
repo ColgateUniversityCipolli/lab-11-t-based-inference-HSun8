6957f95b4ca6da3afd57621689955e8c24e9e30e
HSun8 <24sunh@gmail.com>
2025-04-14 18:22:19 -0400

    task 4 done (code)

--- Lab11.R
+++ Lab11.R
@@ -22,13 +22,13 @@ library(xtable)
 
 # closer vals
 closer.data <- read_csv("closer_vals.csv")
-# farther vals
-farther.data <- read_csv("farther_vals.csv")
+# further vals
+further.data <- read_csv("further_vals.csv")
 
 # consolidate all data
 figG.data <- tibble(closer_vals = closer.data$closer_vals,
-                    farther_vals = farther.data$farther_vals) |>
-  mutate(val_diffs = closer_vals - farther_vals)
+                    further_vals = further.data$further_vals) |>
+  mutate(val_diffs = closer_vals - further_vals)
 # create table for sweave
 xtable(figG.data)
 ################################################################################
@@ -37,73 +37,68 @@ xtable(figG.data)
 library(e1071)
 # part A
 closer.summary <- summarize(figG.data,
-                            mean = mean(closer.vals),
-                            variance = var(closer.vals),
-                            median = median(closer.vals),
-                            IQR = IQR(closer.vals),
-                            skewness = skewness(closer.vals),
-                            e.kurtosis = kurtosis(closer.vals))
+                            mean = mean(closer_vals),
+                            variance = var(closer_vals),
+                            median = median(closer_vals),
+                            IQR = IQR(closer_vals),
+                            skewness = skewness(closer_vals),
+                            e.kurtosis = kurtosis(closer_vals))
 xtable(closer.summary)
 
 # part B
-farther.summary <- summarize(figG.data,
-                            mean = mean(farther.vals),
-                            variance = var(farther.vals),
-                            median = median(farther.vals),
-                            IQR = IQR(farther.vals),
-                            skewness = skewness(farther.vals),
-                            e.kurtosis = kurtosis(farther.vals))
-xtable(farther.summary)
+further.summary <- summarize(figG.data,
+                            mean = mean(further_vals),
+                            variance = var(further_vals),
+                            median = median(further_vals),
+                            IQR = IQR(further_vals),
+                            skewness = skewness(further_vals),
+                            e.kurtosis = kurtosis(further_vals))
+xtable(further.summary)
 # part C
-diffs.summary <- closer.summary - farther.summary
+diffs.summary <- closer.summary - further.summary
 xtable(diffs.summary)
 
 # plot
 ggplot(figG.data)+
-  geom_boxplot(aes(x="Closer", y=closer.vals))+
-  geom_boxplot(aes(x="Farther", y=farther.vals))+
-  geom_boxplot(aes(x="Difference", y=val.diffs))+
+  geom_boxplot(aes(x="Closer", y=closer_vals))+
+  geom_boxplot(aes(x="Further", y=further_vals))+
+  geom_boxplot(aes(x="Difference", y=val_diffs))+
   ylab("delta F/F (%)")
 
 ################################################################################
 # Task 4
-# differ from 0
 
+library(effectsize)
+# part A (closer)
 # closer
-# t statistic
+# mu0 is 0, from phrasing of question
 mu0 <- 0
-x <- figG.data$closer.vals
-(xbar <- mean(x))
+x <- figG.data$closer_vals
 
 # hedge's g + CI
-library(effectsize)
 hedges_g(x = x, mu = mu0, alternative = "greater")
 interpret_hedges_g(1.61)
 
-# p value 
-#(p.val <- pt(q=t.stat, df = n-1))
-
 # t.test
 t.test(x=x, mu = mu0, alternative = "greater")
 
 
-# farther
+# part B (further)
 # t statistic
 mu0 <- 0
-x <- figG.data$farther.vals
+x <- figG.data$further_vals
 
 # hedge's g + CI
-library(effectsize)
-hedges_g(x = x, mu = mu0, alternative = "greater")
+hedges_g(x = x, mu = mu0, alternative = "less")
 interpret_hedges_g(-1.51)
 
 # t.test
-t.test(x=x, mu = mu0, alternative = "greater")
+t.test(x=x, mu = mu0, alternative = "less")
 
 # differences
 # t statistic
 mu0 <- 0
-x <- figG.data$val.diffs
+x <- figG.data$val_diffs
 
 # hedge's g + CI
 hedges_g(x = x, mu = mu0, alternative = "two.sided")
@@ -112,4 +107,63 @@ interpret_hedges_g(1.65)
 # t.test
 t.test(x=x, mu = mu0, alternative = "two.sided")
 
-# QUESTION: When to use two tail, when to use one tail?
+################################################################################
+# Task 5
+ggdat.t <- tibble(t=seq(-5,5,length.out=1000))|>
+  mutate(pdf.null = dt(t, df=n-1))
+# For plotting the observed point
+ggdat.obs <- tibble(t    = t.stat, 
+                    y    = 0) # to plot on x-axis
+
+# Resampling to approximate the sampling distribution 
+# on the data
+R <- 1000
+resamples <- tibble(t=numeric(R))
+for(i in 1:R){
+  curr.sample <- sample(x=x,
+                        size=n,
+                        replace=T)
+  resamples$t[i] = (mean(curr.sample)-mu0)/(sd(curr.sample)/sqrt(n))
+}
+
+t.breaks <- c(-5, qt(0.025, df = n-1), # rejection region (left)
+              0, 
+              qt(0.975, df = n-1), 5,  # rejection region (right)
+              t.stat)                  # t-statistic observed
+xbar.breaks <- t.breaks * s/(sqrt(n)) + mu0
+
+# Create Plot
+ggplot() +
+  # null distribution
+  geom_line(data=ggdat.t, 
+            aes(x=t, y=pdf.null))+
+  geom_hline(yintercept=0)+
+  # rejection regions
+  geom_ribbon(data=subset(ggdat.t, t<=qt(0.025, df=n-1)), 
+              aes(x=t, ymin=0, ymax=pdf.null),
+              fill="grey", alpha=0.5)+
+  geom_ribbon(data=subset(ggdat.t, t>=qt(0.975, df=n-1)), 
+              aes(x=t, ymin=0, ymax=pdf.null),
+              fill="grey", alpha=0.5)+
+  # plot p-value (not visible)
+  geom_ribbon(data=subset(ggdat.t, t>=t.stat), 
+              aes(x=t, ymin=0, ymax=pdf.null),
+              fill="reg", alpha=0.25)+
+  # plot observation point
+  geom_point(data=ggdat.obs, aes(x=t, y=y), color="red")+
+  # Resampling Distribution
+  stat_density(data=resamples, 
+               aes(x=t),
+               geom="line", color="grey")+
+  # clean up aesthetics
+  theme_bw()+
+  scale_x_continuous("t",
+                     breaks = round(t.breaks,2),
+                     sec.axis = sec_axis(~.,
+                                         name = bquote(bar(x)),
+                                         breaks = t.breaks,
+                                         labels = round(xbar.breaks,2)))+
+  ylab("Density")+
+  ggtitle("T-Test for Mean Perceived Whiteness of Social Security Recipients",
+          subtitle=bquote(H[0]==3.5*";"~H[a]!=3.5))
+
